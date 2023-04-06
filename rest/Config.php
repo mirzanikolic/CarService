d4b13c1f1b55c8e5eaf453070c8a28dbb1268be4
Mirza Nikolic <mirza.nikolic@stu.ibu.edu.ba>
2023-04-06 21:28:48 +0200

    DEV-003 Implement base classes to simplify code usage

---
+++ rest/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+class Config {
+
+    public static $host = 'localhost';
+    public static $port = "3306";
+    public static $database = 'carservice';
+    public static $username = 'root';
+    public static $password = '';
+   
+}
+
+?>
