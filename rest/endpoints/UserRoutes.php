702551a168cf0adc14f1b1a3a1122b2da9504fcc
Mirza Nikolic <mirza.nikolic@stu.ibu.edu.ba>
2023-04-06 21:37:57 +0200

    DEV-003 Simplify index.php logic

---
+++ rest/endpoints/UserRoutes.php
@@ -0,0 +1,33 @@
+<?php
+
+Flight::register('userDao', 'UserDao');
+
+Flight::route('GET /staging/users', function() {
+   Flight::json(Flight::userDao()->getUsers());
+});
+
+Flight::route('GET /staging/users/@id', function($id) {
+   Flight::json(Flight::userDao()->getUserById($id));
+});
+
+Flight::route('GET /staging/users/@first_name', function($first_name) {
+    Flight::json(Flight::userDao()->getUserByFirstName($first_name));
+});
+
+Flight::route('POST /staging/users', function(){
+    $data = Flight::request()->data->getData();
+    Flight::json(Flight::userDao()->addUser($data));
+});
+
+Flight::route('PUT /staging/users/@id', function($id) {
+   $data = Flight::request()->data->getData();
+   Flight::userDao()->updateUser($id, $data);
+});
+
+Flight::route('DELETE /staging/users/@id', function($id) {
+   Flight::json(Flight::userDao()->deleteUser($id));
+});
+
+Flight::start();
+
+?>
