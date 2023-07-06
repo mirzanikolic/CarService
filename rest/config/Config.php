34e984427ebe56c142e96eb038ed25195146073a
Mirza Nikolic <mirza.nikolic@stu.ibu.edu.ba>
2023-07-07 00:04:43 +0200

    DEV-005 Add car route

--- rest/config/Config.php
+++ rest/config/Config.php
@@ -2,12 +2,12 @@
 
 class Config {
 
-    public static $host = "localhost";
+    public static $host = "127.0.0.1";
     public static $port = "3306";
     public static $database = "carservice";
     public static $username = "root";
-    public static $password = "";
-   
+    public static $password = "Ciciban123";
+
 }
 
 ?>
