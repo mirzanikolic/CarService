389f2c989c658e6c9636ac9ca02a070904878bd1
Mirza Nikolic <mirza.nikolic@stu.ibu.edu.ba>
2023-07-11 16:21:21 +0200

    DEV-017 Add midleware for routes

--- rest/index.php
+++ rest/index.php
@@ -4,6 +4,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/dao/UserDao.class.php';
 require_once __DIR__.'/services/UserService.php';
@@ -20,6 +23,31 @@ Flight::register('carService', 'CarService');
 
 Flight::register('servicerDao', 'ServicerDao');
 Flight::register('servicerService', 'ServicerService');
+
+// middleware method for login
+Flight::route('/*', function(){
+
+    $key = "kM6GHlqkAofCWH37737pltGEanvJ";
+    //return TRUE;
+    //perform JWT decode
+    $path = Flight::request()->url;
+    if ($path == '/staging/login' || $path == '/docs.json' || '/staging/servicers' || '/staging/users') return TRUE; // exclude login route from middleware
+  
+    $headers = getallheaders();
+    if (@!$headers['Authorization']){
+      Flight::json(["message" => "Authorization is missing"], 403);
+      return FALSE;
+    }else{
+      try {
+        $decoded = (array)JWT::decode($headers['Authorization'], new Key($key, 'HS256'));
+        Flight::set('user', $decoded);
+        return TRUE;
+      } catch (\Exception $e) {
+        Flight::json(["message" => "Authorization token is not valid"], 403);
+        return FALSE;
+      }
+    }
+  });
 
 require_once __DIR__ . '/routes/UserRoutes.php';
 require_once __DIR__ . '/routes/CarRoutes.php';
